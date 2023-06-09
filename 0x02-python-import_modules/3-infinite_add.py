fd3a72ee3a63f9a509769091a032f16eea5b6e7c
Youness-Tr <younesstarwni2003@gmail.com>
2023-06-09 01:27:48 +0100

    rm

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from sys import argv
-if __name__ == "__main__":
-    res = 0
-    for ar in range(1, len(argv)):
-        res = res + int(argv[ar])
-    print("{}".format(res))
